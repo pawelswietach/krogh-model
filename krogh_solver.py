cea80a00c4fcfc625b9a00a042001d7f55b60d2d
pawelswietach <pawel.swietach@dpag.ox.ac.uk>
2026-04-06 00:50:11 +0100

    Add files via upload

--- krogh_solver.py
+++ krogh_solver.py
@@ -17,16 +17,28 @@ def krogh_solver(
     L
 ):
     # --- geometry ---
-    Rcap = 5
+    Rcap = 5.0
     L = float(L)
-    v = 1000
+    v = 1000.0
 
     x = np.linspace(0, L, Nx)
     dx = x[1] - x[0]
 
-    # --- parameters ---
+    # --- volumes ---
     vi = 1 - ve
 
+    # --- diffusion / exchange ---
+    D_free = np.array([2600,2100,1300,10,1000,1000,960,0,0,0], dtype=float)
+    D_t = np.concatenate([D_free[:2], D_free[2:] * ve])
+    h = D_t / (Rcap * np.log((Rtis + Rcap)/Rcap))
+
+    SV_b = 2.0 / Rcap
+    SV_t = 2.0 * Rcap / ((Rcap + Rtis)**2 - Rcap**2)
+
+    K_b = h * SV_b
+    K_t = h * SV_t
+
+    # --- reactions ---
     CA_bld = CA
     CA_tis = CA
 
@@ -47,29 +59,34 @@ def krogh_solver(
 
     # --- inlet ---
     pH_b = 7.4
+    H_b = 10**(-pH_b)
+
     O2_b = startO2 / 1000
     CO2_b = startCO2 / 1000
     HCO3_b = startHCO3 / 1000
     Glu_b = startGlucose / 1000
     Lac_b = 0
     HLac_b = 0
-    pH_b = 7.4  # or make this a GUI input later
-    H_b = 10**(-pH_b)
 
     B_in = np.array([O2_b, CO2_b, HCO3_b, H_b, Lac_b, HLac_b, Glu_b])
+
+    # intracellular initial
+    H_i = 10**(-pHi0)
+    HCO3_i = HCO3_b
+    Lac_i = 0
 
     # --- initial state ---
     U0 = np.zeros((Nx,17))
     for i in range(Nx):
         U0[i,0:7] = B_in
         U0[i,7:14] = B_in
-        U0[i,14] = HCO3_b
-        U0[i,15] = H_b
-        U0[i,16] = 0
+        U0[i,14] = HCO3_i
+        U0[i,15] = H_i
+        U0[i,16] = Lac_i
 
     y0 = U0.flatten()
 
-    # --- RHS ---
+    # ---------- RHS ----------
     def rhs(t, y):
         U = y.reshape(Nx,17)
         dUdt = np.zeros_like(U)
@@ -87,22 +104,59 @@ def krogh_solver(
             O2_b, CO2_b, HCO3_b, H_b, Lac_b, HLac_b, Glu_b = B
             O2_t, CO2_t, HCO3_e, H_e, Lac_e, HLac_t, Glu_t, HCO3_i, H_i, Lac_i = T
 
-            # reactions
+            # --- reactions ---
             rCO2_b = CA_bld*(kr*HCO3_b*H_b - kh*CO2_b)
             rHLac_b = kb*Lac_b*H_b - kf*HLac_b
 
-            # simple advection + reaction only (minimal version)
+            rCO2_e = CA_tis*(kr*HCO3_e*H_e - kh*CO2_t)
+            rHLac_e = kb*Lac_e*H_e - kf*HLac_t
+
+            rCO2_i = CA_tis*(kr*HCO3_i*H_i - kh*CO2_t)
+            rHLac_i = kb*Lac_i*H_i - kf*HLac_t
+
+            # --- metabolism ---
+            O2eff = max(O2_t, 0)
+            mmO2 = O2eff / (O2eff + KmO2)
+
+            Glueff = max(Glu_t, 0)
+            mmG = Glueff / (Glueff + KgG)
+
+            mmH = (10**-7.1)**2.25 / (max(H_i,1e-12)**2.25 + (10**-7.1)**2.25)
+
+            Jresp = JR * mmG * mmO2
+            Jglyc = JG * mmG * mmH
+
+            # --- BLOOD ---
             s_b = np.zeros(7)
 
-            s_b[1] = -v*dUdx[1] + rCO2_b
-            s_b[2] = -v*dUdx[2] - rCO2_b
-            s_b[3] = -v*dUdx[3] - rCO2_b - rHLac_b
+            s_b[0] = K_b[0]*(O2_t - O2_b) - v*dUdx[0]
+            s_b[1] = K_b[1]*(CO2_t - CO2_b) - v*dUdx[1] + rCO2_b
+            s_b[2] = K_b[2]*(HCO3_e - HCO3_b) - v*dUdx[2] - rCO2_b
+            s_b[3] = K_b[3]*(H_e - H_b) - v*dUdx[3] - rCO2_b - rHLac_b
+            s_b[4] = K_b[4]*(Lac_e - Lac_b) - v*dUdx[4] - rHLac_b
+            s_b[5] = K_b[5]*(HLac_t - HLac_b) - v*dUdx[5] + rHLac_b
+            s_b[6] = K_b[6]*(Glu_t - Glu_b) - v*dUdx[6]
+
+            # --- TISSUE ---
+            s_t = np.zeros(10)
+
+            s_t[0] = K_t[0]*(O2_b - O2_t) - 6*vi*Jresp
+            s_t[1] = K_t[1]*(CO2_b - CO2_t) + 6*vi*Jresp + vi*rCO2_i + ve*rCO2_e
+            s_t[2] = K_t[2]*(HCO3_b - HCO3_e) - ve*rCO2_e
+            s_t[3] = K_t[3]*(H_b - H_e) - ve*rCO2_e - ve*rHLac_e
+            s_t[4] = K_t[4]*(Lac_b - Lac_e) - ve*rHLac_e
+            s_t[5] = K_t[5]*(HLac_b - HLac_t) + 2*vi*Jglyc + ve*rHLac_e + vi*rHLac_i
+            s_t[6] = K_t[6]*(Glu_b - Glu_t) - vi*(Jglyc + Jresp)
+            s_t[7] = -vi*rCO2_i
+            s_t[8] = -vi*rCO2_i - vi*rHLac_i
+            s_t[9] = -vi*rHLac_i
 
             dUdt[i,0:7] = s_b
+            dUdt[i,7:17] = s_t
 
         return dUdt.flatten()
 
-    sol = solve_ivp(rhs, [0,2000], y0, method='BDF')
+    sol = solve_ivp(rhs, [0, 2000], y0, method="BDF")
 
     U = sol.y[:,-1].reshape(Nx,17)
 
@@ -111,5 +165,6 @@ def krogh_solver(
         "O2_mM": 1e3*U[:,0],
         "CO2_mM": 1e3*U[:,1],
         "HCO3_mM": 1e3*U[:,2],
-        "pH": -np.log10(U[:,3])
+        "pH": -np.log10(U[:,3]),
+        "Glu_mM": 1e3*U[:,6]
     }
